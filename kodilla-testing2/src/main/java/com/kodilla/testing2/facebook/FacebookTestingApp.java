427528113c9f24daece4047d196df32b79440d25
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-10-12 04:17:29 +0200

    31.2 Created Selenium test for creating acc on Facebook

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,41 @@
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+    public static final String XPATH_COOKIE_BUTTON = "//button[@data-cookiebanner=\"accept_button\"]";
+    public static final String XPATH_REGISTRATION_BUTTON = "//a[@data-testid=\"open-registration-form-button\"]";
+    public static final String XPATH_DIV_BIRTH = "//div[@class=\"mbs _52lq fsl fwb fcb\"]";
+    public static final String XPATH_SELECT_BIRTH_DAY = "//select[@class=\"_9407 _5dba _9hk6 _8esg\"]";
+    public static final String XPATH_SELECT_BIRTH_MONTH = "//select[@class=\"_9407 _5dba _9hk6 _8esg\"]";
+    public static final String XPATH_SELECT_BIRTH_YEAR = "//select[@class=\"_9407 _5dba _9hk6 _8esg\"]";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.facebook.com/");
+
+        WebElement acceptCookies = driver.findElement(By.xpath(XPATH_COOKIE_BUTTON));
+        acceptCookies.click();
+
+        WebElement registerAccount = driver.findElement(By.xpath(XPATH_REGISTRATION_BUTTON));
+        registerAccount.click();
+
+        while (!driver.findElement(By.xpath(XPATH_DIV_BIRTH)).isDisplayed()) ;
+
+        WebElement selectComboBDay = driver.findElement(By.xpath(XPATH_SELECT_BIRTH_DAY));
+        Select selectBDay = new Select(selectComboBDay);
+        selectBDay.selectByValue("10");
+
+        WebElement selectComboBMonth = driver.findElement(By.xpath(XPATH_SELECT_BIRTH_MONTH));
+        Select selectBMonth = new Select(selectComboBMonth);
+        selectBMonth.selectByValue("maj");
+
+        WebElement selectComboBYear = driver.findElement(By.xpath(XPATH_SELECT_BIRTH_YEAR));
+        Select selectBYear = new Select(selectComboBYear);
+        selectBYear.selectByValue("2000");
+    }
+}
