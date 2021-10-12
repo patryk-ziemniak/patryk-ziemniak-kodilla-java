82612aacac475233d3db5ed90dacecd9c6d74bc1
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-10-12 12:59:05 +0200

    31.2 Fix for Selenium test for creating acc on Facebook

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -6,10 +6,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.concurrent.TimeUnit;
+
 public class FacebookTestingApp {
     public static final String XPATH_COOKIE_BUTTON = "//button[@data-cookiebanner=\"accept_button\"]";
     public static final String XPATH_REGISTRATION_BUTTON = "//a[@data-testid=\"open-registration-form-button\"]";
-    public static final String XPATH_DIV_BIRTH = "//div[@class=\"mbs _52lq fsl fwb fcb\"]";
     public static final String XPATH_SELECT_BIRTH_DAY = "//select[@name=\"birthday_day\"]";
     public static final String XPATH_SELECT_BIRTH_MONTH = "//select[@name=\"birthday_month\"]";
     public static final String XPATH_SELECT_BIRTH_YEAR = "//select[@name=\"birthday_year\"]";
@@ -24,18 +25,18 @@ public class FacebookTestingApp {
         WebElement registerAccount = driver.findElement(By.xpath(XPATH_REGISTRATION_BUTTON));
         registerAccount.click();
 
-        while (!driver.findElement(By.xpath(XPATH_DIV_BIRTH)).isDisplayed()) ;
+        driver.manage().timeouts().implicitlyWait(2L, TimeUnit.SECONDS);
 
         WebElement selectComboBDay = driver.findElement(By.xpath(XPATH_SELECT_BIRTH_DAY));
         Select selectBDay = new Select(selectComboBDay);
-        selectBDay.selectByValue("10");
+        selectBDay.selectByVisibleText("10");
 
         WebElement selectComboBMonth = driver.findElement(By.xpath(XPATH_SELECT_BIRTH_MONTH));
         Select selectBMonth = new Select(selectComboBMonth);
-        selectBMonth.selectByValue("maj");
+        selectBMonth.selectByVisibleText("maj");
 
         WebElement selectComboBYear = driver.findElement(By.xpath(XPATH_SELECT_BIRTH_YEAR));
         Select selectBYear = new Select(selectComboBYear);
-        selectBYear.selectByValue("2000");
+        selectBYear.selectByVisibleText("2000");
     }
 }
